7202ce80113f9ae2e43342746c0e6ecf5303a977
ZayThiha <109571154+ZayThihaAung@users.noreply.github.com>
2026-07-19 15:37:59 +0630

    Added database configuration

---
+++ Expense Tracker/script/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://xevlpklmiztoktagsrsp.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_iGwE3Fv8vmrRKYLPHkyUzg_yiOLkGIn';
+
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
