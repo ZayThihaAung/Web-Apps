5fd730b8141d16fb6f87fe912aad456fea1dd854
ZayThihaAung <zaythihaaung2008@gmail.com>
2026-08-06 15:12:23 +0630

    Refactor code structure for improved readability and maintainability

--- script/expense.js
+++ script/expense.js
@@ -1,6 +1,7 @@
 ﻿const listTag = document.querySelector('.list-group');
 const ascendTag = document.querySelector('.ascend');
 const decendTag = document.querySelector('.decend');
+const orderTag = document.querySelector('.dropdown');
 const deletAllExpense = document.querySelector('.btn-delete');
 const canvas = document.querySelector('.expense-chart');
 
@@ -76,8 +77,15 @@ const renderHTML = (expenseList) => {
         });
     }
 
-    listTag.innerHTML = items || `<li class="list-group-item">No expenses added yet.</li>`;
+    listTag.innerHTML = items || `<div class="list-group-item empty-item">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .686-.5A4.5 4.5 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"/>
+    </svg> No expenses found.
+    </div>`;
     canvas.style.display = normalized.length > 0 ? 'block' : 'none';
+    orderTag.style.display = normalized.length > 0? 'block' : 'none';
+    deletAllExpense.style.display = normalized.length > 0? 'block' : 'none';
 };
 
 const sortAndRender = (compareFn) => {
--- script/main.js
+++ script/main.js
@@ -58,6 +58,7 @@ const alertTag5 = `
 const clearExpense = () => {
     alertContainer.innerHTML = '';
     amountTag.value = '';
+    productTag.value = '';
     typeTag.value = '';
 };
 
